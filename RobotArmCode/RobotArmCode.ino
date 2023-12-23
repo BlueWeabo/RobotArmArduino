96e0ecb4e634f37bb7f586c1992a42d2a9de83e4
BlueWeabo <76872108+BlueWeabo@users.noreply.github.com>
2023-12-23 22:53:02 +0200

    working nrf24 module

--- RobotArmCode/RobotArmCode.ino
+++ RobotArmCode/RobotArmCode.ino
@@ -6,87 +6,85 @@
 #include <Stepper.h>
 #include <Servo.h>
 
+// Define amount of steps a motor can do
 #define MOTOR_STEPS 32
-const int MOTOR_BASE[] = {1,2,3,4};
-const int MOTOR_SWIVEL[] = {1,2,3,4};
-const int MOTOR_JOINT_ONE[] = {1,2,3,4};
-const int MOTOR_JOINT_TWO[] = {1,2,3,4};
-const int MOTOR_HAND[] = {1,2,3,4};
-#define SERVO_GRABBER 1
-#define CLK 2
-#define DT 3
-#define IN1 4
-#define IN2 5
-#define IN3 6
-#define IN4 7
 
+// Set pins
+const int MOTOR_BASE[] = {22,23,24,25};
+const int MOTOR_SWIVEL[] = {26,27,28,29};
+const int MOTOR_JOINT_ONE[] = {30,31,32,33};
+const int MOTOR_JOINT_TWO[] = {34,35,36,36};
+const int MOTOR_HAND[] = {37,38,39,40};
+#define SERVO_GRABBER 2
+#define CE 7
+#define CSN 8
+
+// Define commands
+// Define commands
+enum Commands {
+  MOVE_BASE_CW = 1 << 0,
+  MOVE_BASE_CCW = 1 << 1,
+  MOVE_SWIVEL_CW = 1 << 2,
+  MOVE_SWIVEL_CCW = 1 << 3,
+  MOVE_JOINT_ONE_CW = 1 << 4,
+  MOVE_JOINT_ONE_CCW = 1 << 5,
+  MOVE_JOINT_TWO_CW = 1 << 6,
+  MOVE_JOINT_TWO_CCW = 1 << 7,
+  MOVE_HAND_CW = 1 << 8,
+  MOVE_HAND_CCW = 1 << 9,
+  OPEN_HAND = 1 << 10,
+  CLOSE_HAND = 1 << 11,
+  CHANGE_SPEED = 1 << 12
+};
+
+// Set address for NRF24 module
+const byte ADDRESS[5] = "00001";
+
+// Set up NRF24 Module object
+RF24 radio(CE, CSN);
+
+// Define Motor objects
 Stepper baseMotor(MOTOR_STEPS, MOTOR_BASE[0], MOTOR_BASE[2], MOTOR_BASE[1], MOTOR_BASE[3]);
 Stepper swivelMotor(MOTOR_STEPS, MOTOR_SWIVEL[0], MOTOR_SWIVEL[2], MOTOR_SWIVEL[1], MOTOR_SWIVEL[3]);
 Stepper jointOneMotor(MOTOR_STEPS, MOTOR_JOINT_ONE[0], MOTOR_JOINT_ONE[2], MOTOR_JOINT_ONE[1], MOTOR_JOINT_ONE[3]);
 Stepper jointTwoMotor(MOTOR_STEPS, MOTOR_JOINT_TWO[0], MOTOR_JOINT_TWO[2], MOTOR_JOINT_TWO[1], MOTOR_JOINT_TWO[3]);
 Stepper handMotor(MOTOR_STEPS, MOTOR_HAND[0], MOTOR_HAND[2], MOTOR_HAND[1], MOTOR_HAND[3]);
-Servo grabberServo(SERVO_GRABBER);
-
-int previous = 0;
-int counter = 0;
-int currentStateCLK;
-int lastStateCLK;
-String currentDir ="";
-unsigned long lastButtonPress = 0;
+Servo grabber;
 
 void setup() {
+  Serial.begin(9600);
   baseMotor.setSpeed(255);
-	// Set encoder pins as inputs
-	pinMode(CLK,INPUT);
-	pinMode(DT,INPUT);
+  swivelMotor.setSpeed(25);
+  jointOneMotor.setSpeed(255);
+  jointTwoMotor.setSpeed(255);
+  handMotor.setSpeed(255);
+  grabber.attach(SERVO_GRABBER);
 
-	// Setup Serial Monitor
-	//Serial.begin(9600);
-
-	// Read the initial state of CLK
-	lastStateCLK = digitalRead(CLK);
-
-  // attachInterrupt(0, updateEncoder, CHANGE);
-	// attachInterrupt(1, updateEncoder, CHANGE);
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware not responding!"));
+    while (1) {
+      Serial.println("Radio bugged");
+    } // hold program in infinite loop to prevent subsequent errors
+  }
+  radio.openReadingPipe(0, ADDRESS);
+  radio.startListening();
 }
 
 void loop() {
-  // baseMotor.step(STEPS);
-  // return;
-  // Read the current state of CLK
-	currentStateCLK = digitalRead(CLK);
-
-	// If last and current state of CLK are different, then pulse occurred
-	// React to only 1 state change to avoid double count
-	if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
-
-		// If the DT state is different than the CLK state then
-		// the encoder is rotating CCW so decrement
-		if (digitalRead(DT) != currentStateCLK) {
-			baseMotor.step(MOTOR_STEPS);
-      counter--;
-			currentDir ="CCW";
-		} else {
-			// Encoder is rotating CW so increment
-			baseMotor.step(-MOTOR_STEPS);
-      counter++;
-			currentDir ="CW";
-		}
-
-		// Serial.print("Direction: ");
-		// Serial.print(currentDir);
-		// Serial.print(" | Counter: ");
-		// Serial.println(counter);
-	}
-
-	// Remember last CLK state
-	lastStateCLK = currentStateCLK;
-
-  // baseMotor.step(-100);
-
-  delay(1);
-}
-
-void updateEncoder(){
-	
+  if (!radio.available()) {
+    return;
+  }
+	char buffer[32];
+  radio.read(&buffer, sizeof(buffer));
+  int command = buffer[0] * 256 + buffer[1];
+  switch (command) {
+    case MOVE_BASE_CW: {
+      Serial.println("Moving Base CW");
+      break;
+    }
+    case MOVE_BASE_CCW: {
+      Serial.println("Moving Base CW");
+      break;
+    }
+  }
 }
--- RobotArmController/RobotArmController.ino
+++ RobotArmController/RobotArmController.ino
@@ -3,74 +3,200 @@
 #include <RF24.h>
 #include <RF24_config.h>
 
+// Set pins
 #define CE 7
 #define CSN 8
-#define MOTOR_BASE_CLK 2
-#define MOTOR_BASE_DT 3
-#define MOTOR_SWIVEL_CLK 4
-#define MOTOR_SWIVEL_DT 5
-#define MOTOR_JOINT_ONE_CLK 6
-#define MOTOR_JOINT_ONE_DT 7
-#define MOTOR_JOINT_TWO_CLK 9
-#define MOTOR_JOINT_TWO_DT 10
-#define MOTOR_HAND_CLK 11
-#define MOTOR_HAND_DT 12
+#define MOTOR_BASE_SWIVEL_CLK 2
+#define MOTOR_BASE_SWIVEL_DT 3
+#define MOTOR_BASE_SWIVEL_SW 4
+#define MOTOR_JOINTS_CLK 5
+#define MOTOR_JOINTS_DT 6
+#define MOTOR_JOINTS_SW 10
+#define MOTOR_HAND_CLK 0
+#define MOTOR_HAND_DT 1
+#define MOTOR_HAND_SW 9
 #define MOTOR_SPEED A0
-#define GRABBER 13
 
+// Define commands
+enum Commands {
+  MOVE_BASE_CW = 1 << 0,
+  MOVE_BASE_CCW = 1 << 1,
+  MOVE_SWIVEL_CW = 1 << 2,
+  MOVE_SWIVEL_CCW = 1 << 3,
+  MOVE_JOINT_ONE_CW = 1 << 4,
+  MOVE_JOINT_ONE_CCW = 1 << 5,
+  MOVE_JOINT_TWO_CW = 1 << 6,
+  MOVE_JOINT_TWO_CCW = 1 << 7,
+  MOVE_HAND_CW = 1 << 8,
+  MOVE_HAND_CCW = 1 << 9,
+  OPEN_HAND = 1 << 10,
+  CLOSE_HAND = 1 << 11,
+  CHANGE_SPEED = 1 << 12
+};
+
+// Set address for NRF24 module
+const byte ADDRESS[5] = "00001";
+
+// Set up NRF24 Module object
 RF24 radio(CE, CSN);
 
-int previous = 0;
-int counter = 0;
-int currentStateCLK;
-int lastStateCLK;
-String currentDir ="";
+int baseOrSwivel = 0; // Move base by default
+int jointOneOrTwo = 0; // Move joint one by default
+int openedOrClosed = 0; // Hand Opened by default
 
-char address[] = "00001";
+// Variables storing last state and current state of CLK
+int *currentStateBaseSwivelCLK;
+int *lastStateBaseSwivelCLK;
+int *currentStateJointsCLK;
+int *lastStateJointsCLK;
+int *currentStateHandCLK;
+int *lastStateHandCLK;
+
+// Store last speed we set
+int currentSpeed = 0;
 
 void setup() {
-  pinMode(CLK,INPUT);
-  pinMode(DT,INPUT);
+  // Set all the needed pins to INPUT
+  pinMode(MOTOR_BASE_SWIVEL_CLK, INPUT);
+  pinMode(MOTOR_BASE_SWIVEL_DT, INPUT);
+  pinMode(MOTOR_BASE_SWIVEL_SW, INPUT);
+  pinMode(MOTOR_JOINTS_CLK, INPUT);
+  pinMode(MOTOR_JOINTS_DT, INPUT);
+  pinMode(MOTOR_JOINTS_SW, INPUT);
+  pinMode(MOTOR_HAND_CLK, INPUT);
+  pinMode(MOTOR_HAND_DT, INPUT);
+  pinMode(MOTOR_HAND_SW, INPUT);
 
   // Read the initial state of CLK
-  lastStateCLK = digitalRead(CLK);
+  lastStateBaseSwivelCLK = digitalRead(MOTOR_BASE_SWIVEL_CLK);
+  lastStateJointsCLK = digitalRead(MOTOR_BASE_SWIVEL_CLK);
+  lastStateHandCLK = digitalRead(MOTOR_BASE_SWIVEL_CLK);
 
+  // Read initial speed value
+  currentSpeed = map(0, 1023, 0, 255, analogRead(MOTOR_SPEED));
+
+  // Enable Serial communivation for debugging
   Serial.begin(9600);
 
+  // Check if radio has been connected correctly
   if (!radio.begin()) {
     Serial.println(F("radio hardware not responding!"));
-    while (1) {} // hold program in infinite loop to prevent subsequent errors
+    while (1) {
+      Serial.println("Radio bugged");
+    } // hold program in infinite loop to prevent subsequent errors
   }
 
-  radio.openWritingPipe(address);
+  // Open the address of where we will be sending information to
+  radio.openWritingPipe(ADDRESS);
+  // Disable listening to make sure we arne't getting any signals from elsewhere to our transmitter
   radio.stopListening();
+  // Send the speed we have currently 
+  byte buf[3] = {highByte(CHANGE_SPEED), lowByte(CHANGE_SPEED), currentSpeed};
+  radio.write(&buf, sizeof(buf));
 }
 
 void loop() {
-  currentStateCLK = digitalRead(CLK);
-
-  // If last and current state of CLK are different, then pulse occurred
-  // React to only 1 state change to avoid double count
-  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
-
-    // If the DT state is different than the CLK state then
-    // the encoder is rotating CCW so decrement
-    if (digitalRead(DT) != currentStateCLK) {
-      counter--;
-      currentDir ="CCW";
+  int baseSwivelRotation = determineRotationDirection(
+    MOTOR_BASE_SWIVEL_CLK, 
+    MOTOR_BASE_SWIVEL_DT, 
+    lastStateBaseSwivelCLK, 
+    currentStateBaseSwivelCLK);
+  int jointsRotation = determineRotationDirection(
+    MOTOR_JOINTS_CLK, 
+    MOTOR_JOINTS_DT, 
+    lastStateJointsCLK, 
+    currentStateJointsCLK);
+  int handRotation = determineRotationDirection(
+    MOTOR_HAND_CLK, 
+    MOTOR_HAND_DT, 
+    lastStateHandCLK, 
+    currentStateHandCLK);
+  char buffer[3] = "";
+  if (baseSwivelRotation) {
+    if (baseOrSwivel) {
+      if (baseSwivelRotation == -1) {
+        buffer[0] = highByte(MOVE_SWIVEL_CCW);
+        buffer[1] = lowByte(MOVE_SWIVEL_CCW);
+        Serial.println("Moving  Swivel CCW");
+      } else {
+        buffer[0] = highByte(MOVE_SWIVEL_CW);
+        buffer[1] = lowByte(MOVE_SWIVEL_CW);
+        Serial.println("Moving Swivel CW");
+      }
     } else {
-      // Encoder is rotating CW so increment
-      counter++;
-      currentDir ="CW";
+      if (baseSwivelRotation == -1) {
+        buffer[0] = highByte(MOVE_BASE_CCW);
+        buffer[1] = lowByte(MOVE_BASE_CCW);
+        Serial.println("Moving Base CCW");
+      } else {
+        buffer[0] = highByte(MOVE_BASE_CW);
+        buffer[1] = lowByte(MOVE_BASE_CW);
+        Serial.println("Moving Base CW");
+      }
     }
-
-    // Serial.print("Direction: ");
-    // Serial.print(currentDir);
-    // Serial.print(" | Counter: ");
-    // Serial.println(counter);
+    radio.write(&buffer, sizeof(buffer));
   }
 
+  if (jointsRotation) {
+    if (jointOneOrTwo) {
+      if (jointsRotation == -1) {
+        buffer[0] = highByte(MOVE_JOINT_TWO_CCW);
+        buffer[1] = lowByte(MOVE_JOINT_TWO_CCW);
+      } else {
+        buffer[0] = highByte(MOVE_JOINT_TWO_CW);
+        buffer[1] = lowByte(MOVE_JOINT_TWO_CW);
+      }
+    } else {
+      if (jointsRotation == -1) {
+        buffer[0] = highByte(MOVE_JOINT_ONE_CCW);
+        buffer[1] = lowByte(MOVE_JOINT_ONE_CCW);
+      } else {
+        buffer[0] = highByte(MOVE_JOINT_ONE_CW);
+        buffer[1] = lowByte(MOVE_JOINT_ONE_CW);
+      }
+    }
+    radio.write(&buffer, sizeof(buffer));
+  }
+
+  if (handRotation) {
+    if (handRotation == -1) {
+      buffer[0] = highByte(MOVE_HAND_CCW);
+      buffer[1] = lowByte(MOVE_HAND_CCW);
+    } else {
+      buffer[0] = highByte(MOVE_HAND_CW);
+      buffer[1] = lowByte(MOVE_HAND_CW);
+    }
+    radio.write(&buffer, sizeof(buffer));
+  }
+
+  if (currentSpeed != map(0, 1023, 0, 255, analogRead(MOTOR_SPEED))) {
+    currentSpeed = map(0, 1023, 0, 255, analogRead(MOTOR_SPEED));
+    buffer[0] = highByte(MOVE_SWIVEL_CCW);
+    buffer[1] = lowByte(MOVE_SWIVEL_CCW);
+    buffer[2] = currentSpeed;
+    radio.write(&buffer, sizeof(buffer));
+  }
+  
   // Remember last CLK state
-  lastStateCLK = currentStateCLK;
+  lastStateBaseSwivelCLK = *currentStateBaseSwivelCLK;
+  lastStateJointsCLK = *currentStateJointsCLK;
+  lastStateHandCLK = *currentStateHandCLK;
   delay(1);
+}
+
+// Function to check the pins and give back the direction
+// Returns: -1 when rotated CCW, 0 when it wasn't rotated, 1 when rotated CW
+int determineRotationDirection(int clkPin, int dtPin, int *lastState, int *currentState) {
+  currentState = digitalRead(clkPin);
+  if (currentState != lastState  && currentState){
+    // Check whether we are rotating clockwise (CW) or counter-clockwise (CCW)
+    if (digitalRead(MOTOR_BASE_SWIVEL_DT) != currentState) {
+      // if DT pin activated first then we are rotating CCW
+      return -1;
+    } else {
+      // Otherwise it means CLK pin activated first and we are rotating CW
+      return 1;
+    }
+  }
+  return 0;
 }
